f795555fb0b76047640082597a443e54ee15827f
Jessica Brock <jessicabrock03@gmail.com>
2019-12-10 10:21:24 -0800

    sql_queries.py updated

--- Postgres/sql_queries.py
+++ Postgres/sql_queries.py
@@ -2,29 +2,43 @@
 
 import pandas as pd
 import os
+import json
 
 
-postgres_extract = ('''
-  SELECT column_1, column_2, column_3
-  FROM postgres_table
-''')
-
-postgres_insert = ('''
-  INSERT INTO table (column_1, column_2, column_3)
-  VALUES (?, ?, ?)
-''')
-
-# exporting queries
+root_song = 'data/song_data/A'
+root_log = 'data/log_data/2018/11'
 
 
-class SqlQuery:
-    def __init__(self, extract_query, load_query):
-        self.extract_query = extract_query
-        self.load_query = load_query
+def get_data():
+    for subdir, dirs, files in os.walk(root_song):
+        for file in files:
+            parsed = json.loads(file)
+            print(json.dumps(parsed, indent=4, sort_keys=True))
+
+# STOPPED HERE
 
 
-# create instances for SqlQuery class
-postgres_query = SqlQuery(postgres_extract, postgres_insert)
+# postgres_extract = ('''
+#   SELECT column_1, column_2, column_3
+#   FROM postgres_table
+# ''')
 
-# store as list for iteration
-postgres_queries = [postgres_query]
+# postgres_insert = ('''
+#   INSERT INTO table (column_1, column_2, column_3)
+#   VALUES (?, ?, ?)
+# ''')
+
+# # exporting queries
+
+
+# class SqlQuery:
+#     def __init__(self, extract_query, load_query):
+#         self.extract_query = extract_query
+#         self.load_query = load_query
+
+
+# # create instances for SqlQuery class
+# postgres_query = SqlQuery(postgres_extract, postgres_insert)
+
+# # store as list for iteration
+# postgres_queries = [postgres_query]
